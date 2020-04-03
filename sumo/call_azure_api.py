08b5bfa06ad467dd005767d526c5ff3130331495
Lindvar Legran <llag@equinor.com>
2020-04-03 10:53:50 +0200

    call sumo api

--- sumo/call_azure_api.py
+++ sumo/call_azure_api.py
@@ -3,7 +3,7 @@ import requests
 
 
 class CallAzureApi:
-    '''
+    """
         This class can be used for generating an Azure OAuth2 bear token and send a request to Azure JSON rest endpoint.
         The Azure clientId "1826bd7c-582f-4838-880d-5b4da5c3eea2" need to have permissions to the resourceId that you send inn.
 
@@ -11,7 +11,7 @@ class CallAzureApi:
         Parameters
                 resourceId:
                     Need to be an Azure resourceId
-    '''
+    """
 
     tenant = "statoilsrm.onmicrosoft.com"
     authorityHostUrl = "https://login.microsoftonline.com"
@@ -33,17 +33,17 @@ class CallAzureApi:
     def __repr__(self):
         return self.__str__()
 
-    '''
-        Generating an Azure OAuth2 bear token.
-        You need to open this URL in a web browser https://microsoft.com/devicelogin, and enter the code that is printed. 
-        
-        Return 
-                accessToken:
-                    The Bearer Authorization string
-    '''
     def get_bear_token(self):
+        """
+                Generating an Azure OAuth2 bear token.
+                You need to open this URL in a web browser https://microsoft.com/devicelogin, and enter the code that is printed.
 
-        context = AuthenticationContext(self.authority_url, validate_authority=self.tenant, cache=None, api_version=None,
+                Return
+                        accessToken:
+                            The Bearer Authorization string
+        """
+        context = AuthenticationContext(self.authority_url, validate_authority=self.tenant, cache=None,
+                                        api_version=None,
                                         timeout=None, enable_pii=False)
         code = context.acquire_user_code(self.resourceId, self.clientId)
 
@@ -54,19 +54,19 @@ class CallAzureApi:
 
         return self.bearer
 
-    '''
-        Send an request to the url.
-        
-        Parameters
-            url
-                Need to be a Azure rest url that returns a JSON.
-            bearer
-                Optional, if not entered it will generate one by calling the get_bear_token method
-        Return 
-                json:
-                    The json respond from the entered URL
-        '''
     def get_json(self, url, bearer=None):
+        """
+                Send an request to the url.
+
+                Parameters
+                    url
+                        Need to be a Azure rest url that returns a JSON.
+                    bearer
+                        Optional, if not entered it will generate one by calling the get_bear_token method
+                Return
+                        json:
+                            The json respond from the entered URL
+        """
         if bearer is None:
             if self.bearer is None:
                 self.get_bear_token()
@@ -77,5 +77,100 @@ class CallAzureApi:
                    "Authorization": self.bearer}
 
         response = requests.get(url, headers=headers)
+
+        if not response.ok:
+            raise Exception(f'Status code: {response.status_code}, Text: {response.text}')
+
+        return response.json()
+
+    def get_content(self, url, bearer=None):
+        """
+                Send an request to the url.
+
+                Parameters
+                    url
+                        Need to be a Azure rest url that returns a JSON.
+                    bearer
+                        Optional, if not entered it will generate one by calling the get_bear_token method
+                Return
+                        content:
+                            The content respond from the entered URL
+        """
+
+        if bearer is None:
+            if self.bearer is None:
+                self.get_bear_token()
+        else:
+            self.bearer = bearer
+
+        headers = {"Content-Type": "application/json",
+                   "Authorization": self.bearer}
+
+        response = requests.get(url, headers=headers)
+
+        if not response.ok:
+            raise Exception(f'Status code: {response.status_code}, Text: {response.text}')
+
+        return response.content
+
+    def post_json(self, url, blob=None, json=None, bearer=None):
+        """
+                Post binary or json to the url and return the response as json.
+
+                Parameters
+                    url
+                        Need to be a Azure rest url that returns a JSON.
+                    blob
+                        Optional, the binary to save
+                    json
+                        Optional, the json to save
+                    bearer
+                        Optional, if not entered it will generate one by calling the get_bear_token method
+                Return
+                        json:
+                            The json respond from the entered URL
+        """
+        if bearer is None:
+            if self.bearer is None:
+                self.get_bear_token()
+        else:
+            self.bearer = bearer
+
+        headers = {"Content-Type": "application/json",
+                   "Authorization": self.bearer}
+
+        response = requests.post(url, data=blob, json=json, headers=headers)
+
+        if not response.ok:
+            raise Exception(f'Status code: {response.status_code}, Text: {response.text}')
+
+        return response.json()
+
+    def delete_json(self, url, bearer=None):
+        """
+                Send delete to the url and return the response as json.
+
+                Parameters
+                    url
+                        Need to be a Azure rest url that returns a JSON.
+                    bearer
+                        Optional, if not entered it will generate one by calling the get_bear_token method
+                Return
+                        json:
+                            The json respond from the entered URL
+        """
+        if bearer is None:
+            if self.bearer is None:
+                self.get_bear_token()
+        else:
+            self.bearer = bearer
+
+        headers = {"Content-Type": "application/json",
+                   "Authorization": self.bearer}
+
+        response = requests.delete(url, headers=headers)
+
+        if not response.ok:
+            raise Exception(f'Status code: {response.status_code}, Text: {response.text}')
 
         return response.json()
