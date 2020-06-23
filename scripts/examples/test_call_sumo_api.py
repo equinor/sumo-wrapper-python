bd9f85cead90fccb2170e8fc1d31576a2ff47c15
Per Olav Eide Svendsen <peesv@statoil.com>
2020-06-23 14:23:17 +0200

    WIP: Building tests

--- scripts/examples/test_call_sumo_api.py
+++ scripts/examples/test_call_sumo_api.py
@@ -9,30 +9,33 @@ class Connection:
         self.api = CallSumoApi()
         self.api.get_bear_token()        
 
-
 def _upload_parent_object(C, json):
     response = C.api.save_top_level_json(json=json)
-    if response.status_code != 200:
-        raise Exception(response.text)
+    if not 200 <= response.status_code < 202:
+        raise Exception(f'code: {response.status_code}, text: {response.text}')
     parent_id = response.text
     return parent_id
 
 def _upload_blob(C, object_id, blob):
     response = C.api.save_blob(object_id=object_id, blob=blob)
-    if response.status_code != 200:
+    if not 200 <= response.status_code < 202:
         raise Exception(f'blob upload to object_id {object_id} returned {response}')    
     return response.text
 
 def _download_object(C, object_id):
-    json = C.get_json(object_id=object_id)
+    json = C.api.get_json(object_id=object_id)
     return json
 
 def _upload_child_level_json(C, parent_id, json):
     response = C.api.save_child_level_json(object_id=parent_id, json=json)
-    if response.status_code != 200:
+    if not 200 <= response.status_code < 202:
         raise Exception(response.text)
     child_id = response.text
     return child_id
+
+def _delete_object(C, object_id):
+    response = C.api.delete_object(object_id=object_id)
+    return response
 
 
 ##### TESTS #####
@@ -52,21 +55,33 @@ def test_sequence():
     parent_json = {"status": "scratch",
             "field": "JOHAN SVERDRUP",
             "field_guid": 268281971,
-            "testdata_uniquetext": unique_text,
+            "testdata": 'parent',
             "country_identifier": "Norway",
             "some_parent_metadata": {"field1": "1", "field2": "2"}, 
                 "some_ints": {"field3": 3, "field4": 4}, 
                 "some_floats": {"field5": 5.0, "field6": 6.0}
                 }
-    child_json = {"status": "scratch",
+
+    child1_json = {"status": "scratch",
             "field": "JOHAN SVERDRUP",
             "field_guid": 268281971,
-            "testdata_uniquetext": unique_text,
+            "testdata": 'child1',
             "country_identifier": "Norway",
             "some_child_metadata": {"field1": "1", "field2": "2"}, 
                 "some_ints": {"field3": 3, "field4": 4}, 
                 "some_floats": {"field5": 5.0, "field6": 6.0}
                 }
+
+    child2_json = {"status": "scratch",
+            "field": "JOHAN SVERDRUP",
+            "field_guid": 268281971,
+            "testdata": 'child2',
+            "country_identifier": "Norway",
+            "some_child_metadata": {"field1": "1", "field2": "2"}, 
+                "some_ints": {"field3": 3, "field4": 4}, 
+                "some_floats": {"field5": 5.0, "field6": 6.0}
+                }
+
 
     #upload a parent object, get object_id
     parent_id = _upload_parent_object(C=C, json=parent_json)
@@ -76,28 +91,56 @@ def test_sequence():
         _upload_blob(C=C, object_id=parent_id, blob=b)
 
     # upload child object, get child_id
-    child_id = _upload_child_level_json(C=C, parent_id=parent_id, json=child_json)
+    child1_id = _upload_child_level_json(C=C, parent_id=parent_id, json=child1_json)
+    child2_id = _upload_child_level_json(C=C, parent_id=parent_id, json=child2_json)
+    assert child1_id != child2_id
 
-    # upload blob on child level object
-    result = _upload_blob(C=C, object_id=child_id, blob=b)
+    # upload blob on child level
+    _upload_blob(C=C, object_id=child1_id, blob=b)
+    _upload_blob(C=C, object_id=child2_id, blob=b)
 
-    # get uploaded child object JSON
-    objects_results = _download_object(C=C, object_id=child_id)
+    # get child1 JSON
+    objects_results = _download_object(C=C, object_id=child1_id)
     found = objects_results['found']
     if found:
         print('text:')
         print(objects_results)
-        child_sumo_id = objects_results['_id']
     else:
         raise Exception(f'Object not found : {found}')
 
-    # search for child, get one hit
-    search_results = C.api.search('testdata_uniquetext:{}'.format(unique_text))
+    # search for child1, get one hit
+    search_results = C.api.search(query='child1')
     hits = search_results.get('hits').get('hits')
     total = search_results.get('hits').get('total').get('value')
     assert total == 1
     assert len(hits) == 1
 
-    # delete child
+    # search for child2, get one hit
+    search_results = C.api.search(query='child2')
+    hits = search_results.get('hits').get('hits')
+    total = search_results.get('hits').get('total').get('value')
+    assert total == 1
+    assert len(hits) == 1
 
+    # delete child2
+    result = _delete_object(C=C, object_id=child2_id)
+    assert result == 'ok'
+
+    # search for child2, get zero hits
+    search_results = C.api.search(query='child2')
+    hits = search_results.get('hits').get('hits')
+    total = search_results.get('hits').get('total').get('value')
+    assert total == 0
+    assert len(hits) == 0
+
+    # delete parent
+    result = _delete_object(C=C, object_id=parent_id)
+    assert result == 'ok'
+
+    # search for child1, get zero hits
+    search_results = C.api.search('testdata:child1')
+    hits = search_results.get('hits').get('hits')
+    total = search_results.get('hits').get('total').get('value')
+    assert total == 0
+    assert len(hits) == 0
 
