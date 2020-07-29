a334da50f048b45c4183b03261b25cffe611d983
Per Olav Eide Svendsen <peesv@statoil.com>
2020-07-29 13:47:02 +0200

    aligning with master

--- sumo_surface_python/__init__.py
+++
@@ -1,8 +0,0 @@
-from ._fmu import FileOnDisk   #, SurfaceOnDisk, SurfaceOnSumo, SurfacesOnSumo
-from ._fmu import EnsembleOnDisk, EnsembleOnSumo #, EnsemblesOnSumo
-from ._fmu import Sumo
-from ._searcher import SumoSearcher
-#from ._errors import *
-#from ._connection import SumoConnection
-from ._call_sumo_surface_api import CallSumoSurfaceApi
-
