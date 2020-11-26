70a4d1ca5c4cfff915aca15ceb7a2dc84c2dfbad
Rafael Garcia <rafaelgarcia094@gmail.com>
2020-11-26 13:17:40 +0100

    creating a single api method to upload json plus blob

--- tests/test_call_sumo_api.py
+++ tests/test_call_sumo_api.py
@@ -221,6 +221,79 @@ def test_direct_blob_store_upload():
     assert total == 0
 
 
+def test_direct_blob_store_upload_single_operation():
+    """
+        Testing the wrapper functionalities.
+
+        We upload an ensemble object along with a child. After that, we search for
+        those objects to make sure they are available to the user. We then delete
+        them and repeat the search to check if they were properly removed from sumo.
+    """
+    C = Connection()
+    B = b'123456789'
+
+    # Upload Ensemble
+    with open('testdata/fmu_ensemble.yaml', 'r') as stream:
+        fmu_ensemble_metadata = yaml.safe_load(stream)
+
+    response_ensemble = _upload_parent_object(C=C, json=fmu_ensemble_metadata)
+    ensemble_id = response_ensemble.json().get('objectid')
+    fmu_ensemble_id = fmu_ensemble_metadata.get('fmu_ensemble').get('fmu_ensemble_id')
+
+    assert 200 <= response_ensemble.status_code <= 202
+    assert isinstance(response_ensemble.json(), dict)
+
+    # Upload Regular Surface plus BLOB
+    with open('testdata/fmu_regularsurface.yaml', 'r') as stream:
+        fmu_regularsurface_metadata = yaml.safe_load(stream)
+        fmu_regularsurface_metadata['_tests'] = {'test1': 'test'}
+
+    response_surface = C.api.save_regular_surface(ensemble_id, fmu_regularsurface_metadata, B)
+    regular_surface_id = response_surface.json().get('objectid')
+
+    assert 200 <= response_surface.status_code <= 202
+    assert isinstance(response_surface.json(), dict)
+
+    sleep(2)
+
+    # Search for ensemble
+    query = f'fmu_ensemble.fmu_ensemble_id:{fmu_ensemble_id}'
+    search_results = C.api.searchroot(query, select='source', buckets='source')
+    hits = search_results.get('hits').get('hits')
+    assert len(hits) == 1
+    assert hits[0].get('_id') == ensemble_id
+
+    # Search for child object
+    search_results = C.api.search(query='_tests.test1:test')
+    total = search_results.get('hits').get('total').get('value')
+    assert total == 1
+
+    get_result = _download_object(C, object_id=regular_surface_id)
+    assert get_result["_id"] == regular_surface_id
+
+    # Search for blob
+    bin_obj = C.api.get_blob(object_id=regular_surface_id)
+    assert bin_obj == B
+
+    # Delete Ensemble
+    result = _delete_object(C=C, object_id=ensemble_id)
+    assert result == 'Accepted'
+
+    sleep(3)
+
+    # Search for ensemble
+    query = f'fmu_ensemble.fmu_ensemble_id:{fmu_ensemble_id}'
+    search_results = C.api.searchroot(query, select='source', buckets='source')
+
+    hits = search_results.get('hits').get('hits')
+    assert len(hits) == 0
+
+    # Search for child object
+    search_results = C.api.search(query='_tests.test1:test')
+    total = search_results.get('hits').get('total').get('value')
+    assert total == 0
+
+
 def test_fail_on_wrong_metadata():
     """
         Upload a parent object with erroneous metadata, confirm failure
